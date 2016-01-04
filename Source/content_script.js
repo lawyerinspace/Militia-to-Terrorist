c30f78a9a4d19c5bd6fb58714ecd31571a1a1036
lawyerinspace <srookejd@gmail.com>
2016-01-04 03:07:38 +0100

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -4,7 +4,7 @@ function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
 	switch ( node.nodeType )  
@@ -31,12 +31,15 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bMilitia\b/g, "Terrorist");
+	v = v.replace(/\bmilitia\b/g, "terrorist");
+	v = v.replace(/\bMilitias\b/g, "Terrorists");
+	v = v.replace(/\bmilitias\b/g, "terrorists");
+   v = v.replace(/\bantigovernment\b/g, "terrorist");
+   v = v.replace(/\bprotestors\b/g, "terrorists");
+   v = v.replace(/\bprotestor\b/g, "terrorist");
+   v = v.replace(/\boccupation\b/g, "attack");
+
+
 	textNode.nodeValue = v;
 }
-
-
